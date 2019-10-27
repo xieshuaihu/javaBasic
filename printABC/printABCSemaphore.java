584ed219678966e4e71d4042781d50441770db0f
xTiger <2575507562@qq.com>
2019-10-27 16:41:59 +0800

    Update and rename printABC.java to printABC/printABCSemaphore.java

--- printABC/printABCSemaphore.java
+++ printABC/printABCSemaphore.java
@@ -1,6 +1,6 @@
 import java.util.concurrent.Semaphore;
 
-public class printABC {
+public class printABCSemaphore {
     public static void main(String[] args) throws InterruptedException {
         Semaphore sem1 = new Semaphore(1);
         Semaphore sem2 = new Semaphore(1);
